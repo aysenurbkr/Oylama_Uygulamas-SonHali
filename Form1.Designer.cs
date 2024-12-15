7f26cccad99688ce297cf0e330bfd64823e55dd2
ayşenur <aysenurbkr58@gmail.com>
2024-12-15 17:46:43 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxSecenekler = new System.Windows.Forms.ListBox();
+            this.btnOyVer = new System.Windows.Forms.Button();
+            this.btnSonuclariGoster = new System.Windows.Forms.Button();
+            this.btnSifirla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxSecenekler
+            // 
+            this.listBoxSecenekler.FormattingEnabled = true;
+            this.listBoxSecenekler.ItemHeight = 16;
+            this.listBoxSecenekler.Items.AddRange(new object[] {
+            "1.Çok iyi",
+            "2.İdare eder",
+            "3.Orta",
+            "4.Berbat"});
+            this.listBoxSecenekler.Location = new System.Drawing.Point(33, 24);
+            this.listBoxSecenekler.Name = "listBoxSecenekler";
+            this.listBoxSecenekler.Size = new System.Drawing.Size(120, 84);
+            this.listBoxSecenekler.TabIndex = 0;
+            // 
+            // btnOyVer
+            // 
+            this.btnOyVer.Location = new System.Drawing.Point(230, 12);
+            this.btnOyVer.Name = "btnOyVer";
+            this.btnOyVer.Size = new System.Drawing.Size(101, 35);
+            this.btnOyVer.TabIndex = 1;
+            this.btnOyVer.Text = "Oy Ver";
+            this.btnOyVer.UseVisualStyleBackColor = true;
+            // 
+            // btnSonuclariGoster
+            // 
+            this.btnSonuclariGoster.Location = new System.Drawing.Point(230, 53);
+            this.btnSonuclariGoster.Name = "btnSonuclariGoster";
+            this.btnSonuclariGoster.Size = new System.Drawing.Size(102, 39);
+            this.btnSonuclariGoster.TabIndex = 2;
+            this.btnSonuclariGoster.Text = "Sonuçlar:";
+            this.btnSonuclariGoster.UseVisualStyleBackColor = true;
+            // 
+            // btnSifirla
+            // 
+            this.btnSifirla.Location = new System.Drawing.Point(230, 98);
+            this.btnSifirla.Name = "btnSifirla";
+            this.btnSifirla.Size = new System.Drawing.Size(101, 41);
+            this.btnSifirla.TabIndex = 3;
+            this.btnSifirla.Text = "Sıfırla";
+            this.btnSifirla.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSifirla);
+            this.Controls.Add(this.btnSonuclariGoster);
+            this.Controls.Add(this.btnOyVer);
+            this.Controls.Add(this.listBoxSecenekler);
+            this.Name = "Form1";
+            this.Text = "Anket_Oylama_Uygulamasi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxSecenekler;
+        private System.Windows.Forms.Button btnOyVer;
+        private System.Windows.Forms.Button btnSonuclariGoster;
+        private System.Windows.Forms.Button btnSifirla;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, int> secenekler;
+
+        public Form1()
+        {
+            InitializeComponent();
+            secenekler = new Dictionary<string, int>
+            {
+                { "Seçenek 1", 0 },
+                { "Seçenek 2", 0 },
+                { "Seçenek 3", 0 },
+                { "Seçenek 4", 0 }
+            };
+            GuncelleListe();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.btnOyVer.Click += new EventHandler(this.btnOyVer_Click);
+            this.btnSonuclariGoster.Click += new EventHandler(this.btnSonuclariGoster_Click);
+            this.btnSifirla.Click += new EventHandler(this.btnSifirla_Click);
+
+            GuncelleListe();
+        }
+
+        private void GuncelleListe()
+        {
+            listBoxSecenekler.Items.Clear();
+            foreach (var secenek in secenekler)
+            {
+                listBoxSecenekler.Items.Add($"{secenek.Key}: {secenek.Value} oy");
+            }
+        }
+
+        private void btnOyVer_Click(object sender, EventArgs e)
+        {
+            if (listBoxSecenekler.SelectedIndex >= 0)
+            {
+                string secilenSecenek = listBoxSecenekler.SelectedItem.ToString().Split(':')[0].Trim();
+                if (secilenSecenek != "")
+                {
+                    secenekler[secilenSecenek]++;
+                    GuncelleListe();
+                    MessageBox.Show($"{secilenSecenek} için oyunuz kaydedildi!", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir seçenek seçin!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnSonuclariGoster_Click(object sender, EventArgs e)
+        {
+            string sonucMetni = "Sonuçlar:\n";
+            foreach (var secenek in secenekler)
+            {
+                sonucMetni += $"{secenek.Key}: {secenek.Value} oy\n";
+            }
+            MessageBox.Show(sonucMetni, "Sonuçlar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnSifirla_Click(object sender, EventArgs e)
+        {
+            // Anahtarların bir kopyasını alarak işlem yapın
+            var anahtarlar = secenekler.Keys.ToList();
+
+            foreach (var secenek in anahtarlar)
+            {
+                secenekler[secenek] = 0;
+            }
+
+            GuncelleListe();
+            MessageBox.Show("Tüm oylar sıfırlandı.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+    }
+}
